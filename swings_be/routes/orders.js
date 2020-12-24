bcef7b237b7b89926130c6ee2d0b35a605a90008
minhlqd <maiminhlqd@gmail.com>
2020-12-24 17:02:46 +0700

    add

--- swings_be/routes/orders.js
+++ swings_be/routes/orders.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Order = require('../models/order');
+
 router.post('/order', async (req, res) => {
     const order = new Order({
         _id: new mongoose.Types.ObjectId(),
